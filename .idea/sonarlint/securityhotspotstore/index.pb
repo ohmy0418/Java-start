9ffba6ffd595785b6d5050f89ff3708b2ad55832
ohmy04180 <ohmy0418@mobigen.com>
2024-06-03 13:58:50 +0900

    feat: java study 추가

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+D
+src/commentJava.java,2/3/2355fe91a78cf2c70fa20ed0f93987fe505bfc3f
---
+++ src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+
+//    프로그램 실행 시작 시점
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
