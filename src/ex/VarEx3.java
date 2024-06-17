f95b8c781cfaeca9aa3a3ce58cc633fe515c9eb5
ohmy04180 <ohmy0418@mobigen.com>
2024-06-17 16:56:17 +0900

    feat: 변수 수업 끝-

---
+++ src/ex/VarEx3.java
@@ -0,0 +1,10 @@
+package ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        long lonVar = 1000000000L;
+        boolean bool = true;
+        System.out.println(num1);
+        System.out.println(bool);
+    }
+}
---
+++ src/variable/Var5.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var5 {
+    public static void main(String[] args) {
+        int a; // 1. 변수 선언
+        a = 1; // 2. 변수 초기화
+        System.out.println(a);
+
+        int b = 2; // 변수 선언 + 초기화를 한 번에 선언
+        System.out.println(b);
+
+        int c = 3, d = 4; // 여러 변수 선언 + 초기화를 한 번에
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
